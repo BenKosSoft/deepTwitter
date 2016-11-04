ef69b0c693a3b5d5de0edc224e50f10d6a401933
mbenlioglu <mbenlioglu@sabanciuniv.edu>
2016-11-04 08:18:13 +0200

    - Scheduler implemented for committing tweets to database

--- src/main/java/edu/sabanciuniv/deeplearning/repo/TweetRepository.java
+++ src/main/java/edu/sabanciuniv/deeplearning/repo/TweetRepository.java
@@ -1,31 +1,55 @@
 package edu.sabanciuniv.deeplearning.repo;
 
-import javax.persistence.EntityManager;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
+import javax.persistence.EntityManager;
 import edu.sabanciuniv.deeplearning.model.Tweet;
 import edu.sabanciuniv.deeplearning.service.PersistenceManager;
 
 public class TweetRepository {
-	
-	EntityManager em = null;
-	
+
+	private EntityManager em = null;
+
+	private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(3);
+
 	public TweetRepository() {
-		this.em = PersistenceManager.INSTANCE.getEntityManager();	
+		this.em = PersistenceManager.INSTANCE.getEntityManager();
+
+		// Send SET NAMES query for emoji support
 		em.getTransaction().begin();
 		em.createNativeQuery("SET NAMES 'utf8mb4'").executeUpdate();
 		em.getTransaction().commit();
+		em.getTransaction().begin();
+
+		// start the scheduler
+		scheduler.scheduleAtFixedRate(new Runnable() {
+			@Override
+			public void run() {
+				if (!em.isOpen()) {
+					scheduler.shutdown();
+					return;
+				}
+
+				if (em.getTransaction().isActive()){
+					em.getTransaction().commit();
+					em.clear();
+					em.getTransaction().begin();
+				}
+			}
+		}, 1, 1, TimeUnit.MINUTES);
 	}
-	
-	public void addTweet(Tweet tweet){
-		em.getTransaction()
-        .begin();
+
+	public void addTweet(Tweet tweet) {
 		em.persist(tweet);
-//		em.getTransaction()
-//        .commit();
 	}
-	
-	public void closeConnection(){
+
+	public void closeConnection() {
 		em.close();
 		PersistenceManager.INSTANCE.close();
+
+		// close scheduler
+		scheduler.shutdown();
 	}
 }
